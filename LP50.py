580e6a721c6f6c4116143aa721e82e5a26761989
sascha <sascha.bub@xlandscape.org>
2023-09-12 15:52:18 +0200

    Updated version number and documentation

--- LP50.py
+++ LP50.py
@@ -10,6 +10,7 @@ class LP50(base.Component):
     """Calculates the LP50 for a margin of safety analysis."""
     # RELEASES
     VERSION = base.VersionCollection(
+        base.VersionInfo("2.2.6", "2023-09-12"),
         base.VersionInfo("2.2.5", "2023-09-11"),
         base.VersionInfo("2.2.4", "2021-12-10"),
         base.VersionInfo("2.2.3", "2021-11-18"),
@@ -63,6 +64,9 @@ class LP50(base.Component):
     VERSION.changed("2.2.3", "Reports element names of outputs")
     VERSION.changed("2.2.4", "Specifies offset of outputs")
     VERSION.added("2.2.5", "Information on runtime environment")
+    VERSION.changed("2.2.6", "Extended module information for R runtime environment")
+    VERSION.added("2.2.6", "Creation of repository info during documentation")
+    VERSION.added("2.2.6", "Repository info to R runtime environment")
 
     def __init__(self, name, observer, store):
         """
